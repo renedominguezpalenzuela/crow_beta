b543d3b49fe581c3d062aedb6f083a3cfc34c923
Georkis Santiesteban <georkis.santiesteban@mochilav2.jovenclub.cu>
2019-06-09 21:56:03 -0400

    Creando las vistas

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -16,4 +16,28 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
+    /**
+     * @Route("/player-army", name="dashboard_player_army")
+     */
+    public function playerArmyAction()
+    {
+        return $this->render('dashboard/player_army.html.twig');
+    }
+
+    /**
+     * @Route("/kingdom-court", name="dashboard_kingdom_court")
+     */
+    public function kingdomCourtAction()
+    {
+        return $this->render('dashboard/kingdom_court.html.twig');
+    }
+
+    /**
+     * @Route("/world-map", name="dashboard_world_map")
+     */
+    public function worldMapAction()
+    {
+        return $this->render('dashboard/world_map.html.twig');
+    }
 }
